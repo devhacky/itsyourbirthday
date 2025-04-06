7e673e23144ed9e328120a6ef91b95537bc464f1
Aryan Yash <aryanyashishere@gmail.com>
2025-04-06 21:51:28 +0530

    anchals day edit 1

--- file/functions.js
+++ file/functions.js
@@ -35,34 +35,109 @@ $(window).resize(function() {
 	};
 })(jQuery);
 
-function timeElapse(date) {
-    var current = new Date();  // Get current date and time
-    var seconds = (current - date) / 1000;  // Calculate difference in seconds
 
-    var days = Math.floor(seconds / (3600 * 24));  // Calculate total days
-    seconds = seconds % (3600 * 24);  // Remaining seconds after extracting days
+//days calculator
 
-    var hours = Math.floor(seconds / 3600);  // Calculate hours
-    if (hours < 10) {
-        hours = "0" + hours;
+// function timeElapse(date) {
+//     var current = new Date();  // Get current date and time
+//     var seconds = (current - date) / 1000;  // Calculate difference in seconds
+
+//     var days = Math.floor(seconds / (3600 * 24));  // Calculate total days
+//     seconds = seconds % (3600 * 24);  // Remaining seconds after extracting days
+
+//     var hours = Math.floor(seconds / 3600);  // Calculate hours
+//     if (hours < 10) {
+//         hours = "0" + hours;
+//     }
+//     seconds = seconds % 3600;  // Remaining seconds after extracting hours
+
+//     var minutes = Math.floor(seconds / 60);  // Calculate minutes
+//     if (minutes < 10) {
+//         minutes = "0" + minutes;
+//     }
+//     seconds = Math.floor(seconds % 60);  // Remaining seconds
+//     if (seconds < 10) {
+//         seconds = "0" + seconds;
+//     }
+
+//     var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes + "</span> "; 
+//     $("#clock").html(result);
+
+//     var text = "THE WORLD JUST GOT LUCKIER SINCE ";
+//     $("#message-box").html(text);
+// }
+
+
+
+
+
+// birthday countdown
+function timeElapse(targetDate) {
+    var now = new Date();
+    var secondsLeft = (targetDate - now) / 1000;
+
+    if (secondsLeft <= 0) {
+        $("#clock").html("<span class='boom'>🎉 HAPPY BIRTHDAY BABY! 🎂💖</span>");
+        $("#message-box").html("THE WORLD IS CELEBRATING YOU RIGHT NOW!");
+        triggerBoomEffects();  // You can define confetti/fireworks/text animation here
+        return;
     }
-    seconds = seconds % 3600;  // Remaining seconds after extracting hours
 
-    var minutes = Math.floor(seconds / 60);  // Calculate minutes
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    seconds = Math.floor(seconds % 60);  // Remaining seconds
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
+    var days = Math.floor(secondsLeft / (3600 * 24));
+    secondsLeft %= 3600 * 24;
 
-    var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes + "</span> "; 
+    var hours = Math.floor(secondsLeft / 3600);
+    if (hours < 10) hours = "0" + hours;
+    secondsLeft %= 3600;
+
+    var minutes = Math.floor(secondsLeft / 60);
+    if (minutes < 10) minutes = "0" + minutes;
+
+    var seconds = Math.floor(secondsLeft % 60);
+    if (seconds < 10) seconds = "0" + seconds;
+
+    var result = "Time Left: <span class='digit'>" + days + "</span> Days <span class='digit'>" + hours + "</span> Hours <span class='digit'>" + minutes + "</span> Minutes <span class='digit'>" + seconds + "</span> Seconds";
     $("#clock").html(result);
+    $("#message-box").html("Countdown to the most beautiful soul's birthday 🎈");
 
-    var text = "THE WORLD JUST GOT LUCKIER SINCE ";
-    $("#message-box").html(text);
+
+    var birthStart = new Date(2004, 3, 7); // assuming date of birth
+    var today = new Date();
+
+    
+    var totalDays = Math.floor((today - birthStart) / (1000 * 60 * 60 * 24));
+    $("#day-counter").html(totalDays);
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function timeElapse(date){
 // 	var current = new Date();
